60ddd74cba121ae6562c13b1a863362c272b8801
Adam Krone <krone.adam@gmail.com>
2013-09-12 13:54:09 -0500

    Reverts to using wp-config template

--- recipes/default.rb
+++ recipes/default.rb
@@ -121,23 +121,21 @@ log "wordpress_install_message" do
 end
 
 
-unless node['wordpress']['local_repo']
-  template "#{node['wordpress']['dir']}/wp-config.php" do
-    source "wp-config.php.erb"
-    owner "root"
-    group "root"
-    mode "0644"
-    variables(
-      :database        => node['wordpress']['db']['database'],
-      :user            => node['wordpress']['db']['user'],
-      :password        => node['wordpress']['db']['password'],
-      :auth_key        => node['wordpress']['keys']['auth'],
-      :secure_auth_key => node['wordpress']['keys']['secure_auth'],
-      :logged_in_key   => node['wordpress']['keys']['logged_in'],
-      :nonce_key       => node['wordpress']['keys']['nonce']
-    )
-    notifies :write, "log[wordpress_install_message]"
-  end
+template "#{node['wordpress']['dir']}/wp-config.php" do
+  source "wp-config.php.erb"
+  owner "root"
+  group "root"
+  mode "0644"
+  variables(
+    :database        => node['wordpress']['db']['database'],
+    :user            => node['wordpress']['db']['user'],
+    :password        => node['wordpress']['db']['password'],
+    :auth_key        => node['wordpress']['keys']['auth'],
+    :secure_auth_key => node['wordpress']['keys']['secure_auth'],
+    :logged_in_key   => node['wordpress']['keys']['logged_in'],
+    :nonce_key       => node['wordpress']['keys']['nonce']
+  )
+  notifies :write, "log[wordpress_install_message]"
 end
 
 apache_site "000-default" do
